f61c9bd743cb59d3059e4ec4acb04e6fe635624d
EduardoP1907 <eduardo.ps1907@gmail.com>
2023-09-19 20:17:23 -0300

    commit 1

---
+++ src/main/java/com/example/demotingeso/DemotingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotingesoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demotingeso/DemotingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
